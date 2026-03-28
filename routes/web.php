a8908cd7b6cf1724ccc3c2704072bf89d41cfb9d
Sammy Kori <sammykori72@gmail.com>
2026-03-28 02:25:39 +0000

    perf: add inertia ssr to send server rendered HTML to browser onload

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('tasks.index');
-});
+Route::get('/', [TaskController::class, 'index']);
 
 // Generates: index, create, store, edit, update, destroy except show
 Route::resource('tasks', TaskController::class)->except(['show']);
